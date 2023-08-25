d8de60a39d2ad86a734fe6b03d1dfe3048698618
kthomas18 <tkebede@stanford.edu>
2023-08-25 05:32:21 -0700

    new.

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -53,16 +53,7 @@ const Projects = () => {
                 <h2 className='text-4xl font-bold text-center py-8'>Executable Weather Map</h2>
                 <button className='bg-[#00df9a] w-full rounded-md font-medium my-6 mx-auto py-3 text-black' title='Click to see live demo'>Live Demo</button>
             </div>
-            <div className='w-full shadow-xl bg-slate-100 flex flex-col p-10 my-7 rounded-lg hover:scale-105 duration-300'>
-                <img className='w-32 h-32 mx-auto mt-[-3rem] bg-white'src={weather} alt="" />
-                <h2 className='text-4xl font-bold text-center py-8'>Executable Weather Map</h2>
-                <button className='bg-[#00df9a] w-full rounded-md font-medium my-6 mx-auto py-3 text-black' title='Click to see live demo'>Live Demo</button>
-            </div>
-            <div className='w-full shadow-xl bg-slate-100 flex flex-col p-10 my-7 rounded-lg hover:scale-105 duration-300'>
-                <img className='w-32 h-32 mx-auto mt-[-3rem] bg-white'src={weather} alt="" />
-                <h2 className='text-4xl font-bold text-center py-8'>Executable Weather Map</h2>
-                <button className='bg-[#00df9a] w-full rounded-md font-medium my-6 mx-auto py-3 text-black' title='Click to see live demo'>Live Demo</button>
-            </div>
+            
             </Carousel>
         </div>
     </div>
