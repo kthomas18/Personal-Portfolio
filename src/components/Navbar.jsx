45fe2c1a235c67a7a6a262fb0677796487f6d273
kthomas18 <tkebede@stanford.edu>
2023-08-26 15:10:44 -0700

    aws

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,9 +11,6 @@ const handleNav = () => {
 
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
-        <h1 className='w-full text-3xl font-bold text-[#406f60]'>
-            REACT.
-        </h1>
         <ul className='hidden md:flex'>
             <li className='p-4'> 
                 <Link to='/'>Home</Link>
