4758dc4eca40296d41cfc57ba86d79de1169c9d6
kthomas18 <tkebede@stanford.edu>
2023-08-26 23:52:30 -0700

    new

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,15 +4,14 @@ const Newsletter = () => {
   return (
     <div className='w-full py-16 text-white px-4'>
         <div className='max-w-[1240px] mx-auto grid lg:grid-cols-3'>
-            <div className='lg:col-span-2 my-4'>
-                <h1 className='md:text-4xl sm:text-3xl text-2xl py-2 font-bold'> What's Next? </h1>
+            <div className='lg:col-span-4 my-4'>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl py-2 font-bold my-10'> What's Next? </h1>
                 <p> I am always interested in new projects and opportunities or discussing my past work! The best way to contact me is by email, or shoot me a message on Linkedin!</p>
             </div>
-            <div className='my-4'>
-                <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
-                    <input className='p-3 flex w-full rounded-md text-black' type="email" placeholder='Enter Email' />
+            <div className='flex flex-col sm:flex-row items-center justify-between w-full'>
+                <a href="https://www.linkedin.com/in/thomaskebede">
                     <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto ml-4 px-6 py-3 text-black'>Notify Me</button>
-                </div>
+                </a>
             </div>
         </div>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -15,16 +15,16 @@ const handleNav = () => {
     <div className='flex justify-between items-center h-24 max-w-[1400px] mx-auto px-4 text-white'>
         <img className='mx-[-10]' src={logo} alt="" />
         <ul className='hidden md:flex'>
-            <li className='p-4'> 
+            <li className='p-4 text-2xl'> 
                 <Link to='/'>Home</Link>
             </li>
-            <li className='p-4'>
+            <li className='p-4 text-2xl'>
                 <Link to='/projects'>Projects</Link>
             </li>
-            <li className='p-4'>
+            <li className='p-4 text-2xl'>
                 <Link to='/about'>About</Link>
             </li>
-            <li className='p-4'>
+            <li className='p-4 text-2xl'>
                 <Link to='/contact'>Contact</Link>
             </li>
         </ul>
@@ -32,19 +32,27 @@ const handleNav = () => {
             {nav ? <AiOutlineClose size={35}/> : <AiOutlineMenu size={35} />}
         </div>
         <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-black ease-in-out duration-500' : 'fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+        <img className='mx-[-10]' src={logo} alt="" />
             <ul className='uppercase p-4'>
             <li className='p-4 border-b border-gray-500'>
-                <Link to='/'>Home</Link>
+                <Link to='/'>
+                    Home
+                </Link>
             </li>
             <li className='p-4 border-b border-gray-500'>
-                <Link to='/projects'>Projects</Link>
+                <Link to='/projects'>
+                    Projects
+                </Link>
             </li>
             <li className='p-4 border-b border-gray-500'>
-                <Link to='/about'>About</Link>
+                <Link to='/about'>
+                    About
+                </Link>
             </li>
             <li className='p-4 border-b border-gray-500'>
-                <Link to='/contact'>Contact</Link>
+                <Link to='/contact'>
+                    Contact
+                </Link>
             </li>
             </ul>
         </div>
